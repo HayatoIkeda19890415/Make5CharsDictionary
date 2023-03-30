854157d2c2d66c5cbf11caba1a8f716aa714a1ba
Hayato Ikeda <95620738+HayatoIkeda19890415@users.noreply.github.com>
2023-03-30 19:30:31 +0900

    add create file step

--- Make5CharsDictionary/Program.cs
+++ Make5CharsDictionary/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+const string PATH_DICT = @".\Dictionary\";
+if (!Directory.Exists(PATH_DICT))
+{
+    Directory.CreateDirectory(PATH_DICT);
+}
+
+for (char a = 'a'; a <= 'z'; a++)
+{
+    string filepath = PATH_DICT + a + ".txt";
+    if (File.Exists(filepath))
+    {
+        Console.WriteLine(filepath + " Exists. Please check the file.");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+    else
+    {
+        File.Create(PATH_DICT + a + ".txt");
+    }
+}
