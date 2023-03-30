9344bcbae477e0feb5b856df6ac76e0adfafc1a4
Hayato Ikeda <95620738+HayatoIkeda19890415@users.noreply.github.com>
2023-03-30 19:47:06 +0900

    add directory and input existence check

--- Make5CharsDictionary/Program.cs
+++ Make5CharsDictionary/Program.cs
@@ -2,7 +2,17 @@
 // Console.WriteLine("Hello, World!");
 
 const string PATH_DICT = @".\Dictionary\";
-if (!Directory.Exists(PATH_DICT))
+if (Directory.Exists(PATH_DICT))
+{
+    Console.WriteLine(PATH_DICT + " Exists. Can I delete directory? y/else");
+    string? input = Console.ReadLine();
+    if (input != null && input.ToLower() == "y")
+    {
+        Directory.Delete(PATH_DICT);
+        Directory.CreateDirectory(PATH_DICT);
+    }
+}
+else
 {
     Directory.CreateDirectory(PATH_DICT);
 }
@@ -20,4 +30,12 @@ for (char a = 'a'; a <= 'z'; a++)
     {
         File.Create(PATH_DICT + a + ".txt");
     }
+}
+
+const string PATH_INPUT = @".\input\words.txt";
+if (!File.Exists(PATH_INPUT))
+{
+    Console.WriteLine(PATH_INPUT + " doesn't exists. Please check the path.");
+    Console.ReadLine();
+    Environment.Exit(0);
 }
